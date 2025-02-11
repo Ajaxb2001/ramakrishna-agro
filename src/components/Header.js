b7ad97c9eeff4cd505a2c9cb113f27edd0634b6e
Ajay Bashetti <83651951+Ajaxb2001@users.noreply.github.com>
2025-02-11 22:01:39 +0000

    Added Hamburger

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,34 @@
 // Header.js
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../css/Header.css';
 import logo from '../assets/logo.jpeg';
 
 const Header = () => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <header className="header">
       <div className="header-logo">
-        <Link to="/"> {/* Wrap the logo in a Link to navigate to Productlist */}
+        <Link to="/">
           <img src={logo} alt="Ramakrishna Agro Logo" className="logo" />
         </Link>
         <h1 className="company-name">Ramakrishna Agro</h1>
       </div>
-      <nav className="nav">
+      <nav className={`nav ${isSidebarOpen ? 'open' : ''}`}>
         <Link to="/" className="nav-link">Products</Link>
         <Link to="/about" className="nav-link">About Us</Link>
         <Link to="/contact" className="nav-link">Contact</Link>
       </nav>
+      <div className="hamburger" onClick={toggleSidebar}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
+      </div>
     </header>
   );
 };
